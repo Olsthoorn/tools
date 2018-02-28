2bcb0b710d9db414fcb7b9471b94914d37af07d3
Theo Olsthoorn <tolsthoorn@gmail.com>
2018-02-28 02:28:29 +0100

    HFB toegevoegd and mfgrid

--- fdm/mfgrid.py
+++ fdm/mfgrid.py
@@ -544,6 +544,77 @@ class Grid:
         return LRC[:,0] * self.ny * self.nx + LRC[:, 1] * self.nx + LRC[:, 2]
 
 
+    def cell_pairs(self, polyline, open=False):
+        '''return cell pairs left and right of polygon contour like for HB package.
+
+        parameters
+        ----------
+            gr: fdm.mfgrid.Grid
+                grid object
+            polygon: list of coordinate pairs or a XY array [n, 2]
+                contour coordinates
+            open: True|False
+                use open=True for an open polyline instead of a closed polygon
+
+        >>> x = np.linspace(-100., 100., 21)
+        >>> y = np.linspace(-100., 100., 21)
+        >>> z = [0, -10, -20]
+        >>>
+        >>> gr = Grid(x, y, z)
+        >>>
+        >>> polygon = np.array([(23, 15), (45, 50.), (10., 81.), (-5., 78), (-61., 51.), (-31., 11.),
+        >>>            (-6., -4.), (-42., -20.), (-50., -63.), (-7., -95.),
+        >>>            (31., -80.), (60., -71.), (81., -31.), (5., -63.), (25., -15.), (95., 40.),
+        >>>            (23, 15)])
+        >>>
+        >>> pairs = cell_pairs(gr, polygon)
+        >>>
+        >>>
+        >>> fig, ax = plt.subplots()
+        >>> ax.set_title('Node pairs for the hor. flow-barrier package of Modflow')
+        >>> ax.set_xlabel('x [m]')
+        >>> ax.set_ylabel('y [m]')
+        >>> gr.plot_grid(world=False, ax=ax)
+        >>> ax.plot(polygon[:,0], polygon[:,1])
+        >>> ax.plot(gr.Xm.ravel()[pairs[:,0]], gr.Ym.ravel()[pairs[:,0]], '.r', label='column 1')
+        >>> ax.plot(gr.Xm.ravel()[pairs[:,1]], gr.Ym.ravel()[pairs[:,1]], '.b', label='column 2')
+        >>> for pair in pairs:
+        >>>     ax.plot(gr.Xm.ravel()[pair], gr.Ym.ravel()[pair], 'k-')
+
+        '''
+        A = 1e8 # np.Inf does not work
+
+        if open==True:
+            polygon_west = np.vstack(( np.array([-A, polyline[0][1]]),
+                                       polyline,
+                                       np.array([-A, polyline[-1][1]])))
+            polygon_south = np.vstack((np.array([polyline[0][0], -A]),
+                                       polyline,
+                                       np.array([polyline[-1][0], -A])))
+            In1 = self.inpoly(polygon_west)
+            mask_west  = np.hstack((In1, np.zeros((self.ny, 1), dtype=bool)))
+            In2 = self.inpoly(polygon_south)
+            mask_south = np.vstack((np.zeros((1, self.nx), dtype=bool), In2))
+        else:
+            if not np.all(polyline[0] == polyline[-1]):
+                polyline = np.vstack((polyline, polyline[-1:, :]))
+            In = self.inpoly(polygon)
+            mask_west  = np.hstack((In, np.zeros((self.ny, 1), dtype=bool)))
+            mask_south = np.vstack((np.zeros((1, self.nx), dtype=bool), In))
+
+        west  = np.abs(np.diff(mask_west , axis=1)) == 1
+        south = np.abs(np.diff(mask_south, axis=0)) == 1
+
+        east = np.hstack((np.zeros((self.ny, 1), dtype=bool), west[:,:-1]))
+        north= np.vstack((south[1:,:], np.zeros((1, self.nx), dtype=bool)))
+
+        pairs = np.array([np.hstack((self.NOD[0][west], self.NOD[0][north])),
+                          np.hstack((self.NOD[0][east], self.NOD[0][south]))]).T
+
+        return pairs
+
+
+
     @property
     def dx(self):
         '''Vector [nx] of column widths'''
@@ -2068,5 +2139,33 @@ def sub_grid(rdr, dx=0.1, dz=0.1, dy=1.):
 
 
 if __name__ == '__main__':
-    unittest.main()
+    #unittest.main()
 
+
+    # Pairs for the HDF package
+    x = np.linspace(-100., 100., 21)
+    y = np.linspace(-100., 100., 21)
+    z = [0, -10, -20]
+
+    gr = Grid(x, y, z)
+
+    polygon = np.array([(23, 15), (45, 50.), (10., 81.), (-5., 78), (-61., 51.), (-31., 11.),
+               (-6., -4.), (-42., -20.), (-50., -63.), (-7., -95.),
+               (31., -80.), (60., -71.), (81., -31.), (5., -63.), (25., -15.), (95., 40.),
+               (23, 15)])
+
+    pairs = gr.cell_pairs(polygon, open=False) # a closed polygon
+    polygon = polygon[:10]
+    pairs = gr.cell_pairs(polygon, open=True) # an open line
+
+
+    fig, ax = plt.subplots()
+    ax.set_title('Node pairs for the hor. flow-barrier package of Modflow')
+    ax.set_xlabel('x [m]')
+    ax.set_ylabel('y [m]')
+    gr.plot_grid(world=False, ax=ax)
+    ax.plot(polygon[:,0], polygon[:,1])
+    ax.plot(gr.Xm.ravel()[pairs[:,0]], gr.Ym.ravel()[pairs[:,0]], '.r', label='column 1')
+    ax.plot(gr.Xm.ravel()[pairs[:,1]], gr.Ym.ravel()[pairs[:,1]], '.b', label='column 2')
+    for pair in pairs:
+        ax.plot(gr.Xm.ravel()[pair], gr.Ym.ravel()[pair], 'k-')
